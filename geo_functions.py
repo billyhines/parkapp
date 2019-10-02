f42c2f53b8d175caef7c51b09046c1983ef0e777
Billy Hines <bhines239@gmail.com>
2019-10-02 16:42:50 -0400

    Got user inputted address geocoded, plotted, and the close spots plotted

--- geo_functions.py
+++ geo_functions.py
@@ -6,3 +6,20 @@ def geocode_address(locationQuery):
     coordinates = [location.longitude, location.latitude]
 
     return(coordinates)
+
+def findCloseParking(point, meters):
+    from pymongo import MongoClient
+    client = MongoClient()
+    db = client['parking']
+
+    closeBays = db.bayData.find({  'geometry': {
+                                     '$near': {
+                                       '$geometry': {
+                                          'type': "Point" ,
+                                          'coordinates': [point[0], point[1]]
+                                       },
+                                       '$maxDistance': meters
+                                     }
+                                   }
+                                })
+    return(closeBays)
