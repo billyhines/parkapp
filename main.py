6e13649736a3cf4bacb610761f63a518349ea2c7
Billy Hines <bhines239@gmail.com>
2019-11-27 16:48:30 -0500

    Combined address and parking into single route and javascript function

--- main.py
+++ main.py
@@ -5,31 +5,15 @@ import geo_functions
 app = Flask(__name__)
 app.secret_key = 'super secret'
 
-@app.route("/address-point", methods=['POST'])
-def get_user_point():
-    address = request.get_json()['address']
-
-    features = []
-    user_point = geo_functions.geocode_address(address)
-
-    features.append({"type": "Feature",
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": user_point["coordinates"]},
-                        "properties": {
-                            "cleanAddress": user_point["address"]
-                        }})
-
-    return jsonify(features)
-
-@app.route("/nearby-parking", methods=['POST'])
-def get_parking_data():
+@app.route("/address-and-parking", methods=['POST'])
+def get_address_and_parking():
     client = MongoClient()
 
     address = request.get_json()['address']
     time = request.get_json()['time']
 
-    features = []
+    address_data = []
+    space_data = []
 
     try:
         user_point = geo_functions.geocode_address(address)
@@ -37,13 +21,24 @@ def get_parking_data():
     except ValueError as e:
         return jsonify({"message": e.message}), 400
 
+    address_data.append({"type": "Feature",
+                            "geometry": {
+                                "type": "Point",
+                                "coordinates": user_point["coordinates"]},
+                            "properties": {
+                                "cleanAddress": user_point["address"]
+                        }})
+
     blockCoords = geo_functions.findBlockCoordinates(closeBlocks, client)
-    features.extend(blockCoords)
-    featuresWithPrediction = geo_functions.getBlockAvailability(features, time, client)
+    space_data.extend(blockCoords)
+    space_data = geo_functions.getBlockAvailability(space_data, time, client)
+
+    mapping_data = {"address_data": address_data,
+                    "space_data" : space_data}
 
     client.close()
 
-    return jsonify(featuresWithPrediction)
+    return jsonify(mapping_data)
 
 @app.route('/')
 def main():
