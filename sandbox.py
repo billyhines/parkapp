f026e218ee8704d3888c5dfe02d48c9a900d32fa
Billy Hines <bhines239@gmail.com>
2019-12-11 05:25:42 -0500

    removing pyc files

--- sandbox.py
+++ sandbox.py
@@ -1,32 +1,43 @@
-import pandas as pd
-import datetime
-from dateutil.parser import parse
-from pymongo import MongoClient
-
-import geo_functions
-import cProfile
-
-
-#address = '252 Swanston St Melbourne Australia'
-address = '818 Bourke St Melbourne Australia'
-time = '2019-11-28T22:20'
-
-features = []
-user_point = geo_functions.geocode_address(address)
-closeBlocks = geo_functions.findCloseBlocks(user_point["coordinates"], 150)
-features.extend(closeBlocks)
-featuresWithPrediction = geo_functions.getBlockAvailability(features, time)
-
-prof = cProfile.run("""
-""", sort=2)
-
-
-# Can't geolocate
-address = 'trashjinput_wontlocat'
-user_point = geo_functions.geocode_address(address)
-
-# Can't find close blocks
-
-address = '101 Arch Street Boston, MA'
-user_point = geo_functions.geocode_address(address)
-closeBlocks = geo_functions.findCloseBlocks(user_point["coordinates"], 150)
+# import pandas as pd
+# import numpy as np
+# import datetime
+# from dateutil.parser import parse
+# from pymongo import MongoClient
+# import itertools
+#
+# from flask import jsonify
+# import geo_functions
+# import cProfile
+# from pymongo import MongoClient
+#
+# address = '818 Bourke St Melbourne Australia'
+# time = '2019-11-28T22:20'
+#
+# client = MongoClient()
+#
+#
+#
+# address_data = []
+# space_data = []
+#
+# user_point = geo_functions.geocode_address(address)
+# closeBlocks = geo_functions.findCloseBlocks(user_point["coordinates"], 150, client)
+# type(closeBlocks)
+#
+#
+# address_data.append({"type": "Feature",
+#                         "geometry": {
+#                             "type": "Point",
+#                             "coordinates": user_point["coordinates"]},
+#                         "properties": {
+#                             "cleanAddress": user_point["address"]
+#                     }})
+#
+# blockCoords = geo_functions.findBlockCoordinates(closeBlocks, client)
+# space_data.extend(blockCoords)
+# space_data = geo_functions.getBlockAvailability(space_data, time, client)
+#
+# timestamp = datetime.datetime.now()
+#
+# timestamp = datetime.datetime(2017, timestamp.month, timestamp.day, timestamp.hour, timestamp.minute)
+# type(timestamp)
