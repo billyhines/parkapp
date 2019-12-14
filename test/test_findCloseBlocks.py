6e476f51f96006262adb1c7c95b43dd8fab956b8
Billy Hines <bhines239@gmail.com>
2019-12-14 12:40:31 -0500

    Added test for ValueError

--- test/test_findCloseBlocks.py
+++ test/test_findCloseBlocks.py
@@ -2,12 +2,12 @@ import sys, os
 sys.path.append(os.path.abspath(os.path.join('..')))
 from geo_functions import findCloseBlocks
 
+import pytest
+import pandas as pd
+from pymongo import MongoClient
+client = MongoClient()
+
 def test_answer():
-    import pandas as pd
-    from pymongo import MongoClient
-
-    client = MongoClient()
-
     expected_blocks = pd.DataFrame({'StreetName': ['BOURKE STREET', 'BOURKE STREET', 'MERCHANT STREET',
                                                    'BOURKE STREET', 'IMPORT LANE', 'BOURKE STREET',
                                                    'ENTERPRIZE WAY', 'BOURKE STREET', 'MERCHANT STREET'],
@@ -23,3 +23,9 @@ def test_answer():
     testing_blocks.reset_index(inplace=True, drop = True)
 
     pd.testing.assert_frame_equal(expected_blocks, testing_blocks)
+
+def test_error():
+    with pytest.raises(ValueError, match="No parking bays found near specified point"):
+        test_coords = [-71.064179, 42.356550]
+        test_meters = 100
+        findCloseBlocks(test_coords, test_meters, client)
